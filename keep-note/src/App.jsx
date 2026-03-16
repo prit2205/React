77285688f30814e7ecf2e9610988ed5fe3ff2225
prit2205 <pritpatel2500@gmail.com>
2026-03-16 17:44:00 +0530

    first commit

--- keep-note/src/App.jsx
+++ keep-note/src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <>
-    
+    <section className='lg:flex items-center justify-center'>
+    {/* form for add new task */}
+    <div className='lg:w-1/2 w-full h-screen'></div>
+
+    {/* div for show all task */}
+     <div className='lg:w-1/2 w-full h-screen'></div>
+    </section>
     </>
   )
 }
