37bc5bf63b2222fa0a85e49a452504db851a95a1
prit2205 <pritpatel2500@gmail.com>
2026-03-20 17:06:54 +0530

    first commit

---
+++ useeffects/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ApiFetch from './Components/ApiFetch'
+import UseEffect from './Components/UseEffect'
+
+const App = () => {
+  return (
+   <>
+   {/* <ApiFetch /> */}
+
+   <UseEffect />
+   </>
+  )
+}
+
+export default App
+
+
