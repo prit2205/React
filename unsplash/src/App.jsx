7f9e2b8a68336ddc70d4e6d57c68d2e06c3689e9
prit2205 <pritpatel2500@gmail.com>
2026-03-24 17:05:35 +0530

    first commit

--- unsplash/src/App.jsx
+++ unsplash/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Homepage from './Pages/Homepage'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Pages/Login'
+import SideBar from './Components/SideBar'
+
 
 const App = () => {
   return (
     <>
-    <Homepage />
-    
+    <SideBar />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      </Routes>    
     </>
   )
 }
